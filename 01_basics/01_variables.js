70c4d81232033082812d9630c9844e57321319e2
swapnilpandit101 <134355873+swapnilpandit101@users.noreply.github.com>
2024-08-31 15:05:09 +0000

    learnt a variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "dev@123.com"
+var accountPassword = "12345"
+accountCity = "nanded"
+let accountState
+
+// accountId = 1;   // we camt re asign the value to the constant variables
+
+
+accountEmail = "swap@123.com"
+accountPassword = "54321"
+accountCity = "pune"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
