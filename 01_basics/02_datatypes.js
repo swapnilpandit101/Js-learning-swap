fb43fe34e31d3775c70651783fce8dbadb91d8f8
swapnilpandit101 <134355873+swapnilpandit101@users.noreply.github.com>
2024-09-02 13:29:13 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"
+console.log("swapnil pandit");
+                               // alert(3+3)  we cant use it in node it has propr way o use alert in nodejs 
+console.log(3+3)
+console.log("nanded");   // we can separate statement by ; 
+
+let name = "deva"
+let age = 25
+let isLoggedIn = true
+
+// numbe datatype        => 2 to powe 53
+// bigint datatype
+// string datatype       => double quote ""
+// boolean               => true and false
+// null                  => it is a standalone value and datatype also, we can asign null value to variable
+// undefined             =>  we can asign undefined value to variable
+// symbol                => unique
+// Object                => big topic
+
+
+console.log(typeof"dewa");          // string
+console.log(typeof age);            // number
+console.log(typeof null);           // object
+console.log(typeof undefined);      // undefined
+console.log(typeof isLoggedIn);     // Boolean
